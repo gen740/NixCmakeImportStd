ef2523928e7e0985669f020adc6f5a2525576448
gen740 <54583542+gen740@users.noreply.github.com>
2025-02-15 21:59:01 +0900

    refactor(nix): Delete unused overlay code

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repository provides an example of using CMake's `import std` feature within
 Using `import std` with CMake is still a work in progress and comes with numerous unresolved issues. The following known issues may significantly impact the setup, making it challenging to use reliably:
 
 - [LLVM Issue #120215](https://github.com/llvm/llvm-project/issues/120215)
+- [LLVM Issue 121709](https://github.com/llvm/llvm-project/issues/121709)
 - [CMake Issue #25965](https://gitlab.kitware.com/cmake/cmake/-/issues/25965)
 - [Nixpkgs Issue #370217](https://github.com/NixOS/nixpkgs/issues/370217)
 
--- flake.nix
+++ flake.nix
@@ -13,18 +13,6 @@
         overlays = [
           (final: prev: {
             llvmPackages_19 = prev.llvmPackages_19 // {
-              libcxxStdenv = prev.llvmPackages_19.libcxxStdenv.override {
-                stdenv = prev.llvmPackages_19.stdenv.override {
-                  libc = prev.llvmPackages_19.libc; # LLVM libc を指定
-                };
-              };
-            };
-            libcxxClang = prev.llvmPackages_19.libcxxClang.override {
-              stdenv = final.llvmPackages_19.libcxxStdenv;
-            };
-          })
-          (final: prev: {
-            llvmPackages_19 = prev.llvmPackages_19 // {
               clang-tools = prev.llvmPackages_19.clang-tools.override { enableLibcxx = true; };
             };
           })
